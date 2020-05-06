a1df1ed84db4c9c077add2567adb6bd9b7ea51b1
Sai_Sumanth Lokinindi <saisumanthlokinindi@gmail.com>
2020-05-07 02:59:59 +0530

    Init Commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const connectionURL = process.env.MONGODB_URL;
+
+mongoose.connect(connectionURL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+})
